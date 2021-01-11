bfb12cfbf51c99e97269c33f9aac6a42d415f5a6
Dave <davekim0203@gmail.com>
2021-01-11 03:23:28 -0500

    Add flag icons to lottery dropdown

--- app/src/main/java/org/dave/lotterysimulatorwithstat/adapter/NoFilterArrayAdapter.kt
+++ app/src/main/java/org/dave/lotterysimulatorwithstat/adapter/NoFilterArrayAdapter.kt
@@ -1,8 +1,12 @@
 package org.dave.lotterysimulatorwithstat.adapter
 
 import android.content.Context
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Filter
+import android.widget.TextView
+import org.dave.lotterysimulatorwithstat.R
 
 class NoFilterArrayAdapter<T>(
     context: Context,
@@ -28,5 +32,17 @@ class NoFilterArrayAdapter<T>(
         override fun publishResults(arg0: CharSequence, arg1: FilterResults) {
             notifyDataSetChanged()
         }
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val v = super.getView(position, convertView, parent)
+        val tv = v.findViewById<TextView>(android.R.id.text1)
+        tv.setCompoundDrawablesWithIntrinsicBounds(
+            when (position) {
+                0, 1 -> R.drawable.ic_united_states_of_america
+                else -> R.drawable.ic_canada
+            }, 0, 0, 0
+        )
+        return v
     }
 }
